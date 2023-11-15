e3b3346ae200d3f8eb714ad47b4be2bd6c9e4463
RuslanSalimov <mail-spam2012@yandex.ru>
2023-11-15 16:28:52 +0500

    4th task complete

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,4 +1,4 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
@@ -14,4 +14,18 @@ function fetchMeetupById(meetupId) {
   });
 }
 
-// Требуется создать Vue приложение
+const App = defineComponent({
+  data() {
+    return {
+      rangeId: null,
+      meetup: null
+    }
+  },
+  watch:{
+    async rangeId(oldValue, newValue) {
+      this.meetup = await fetchMeetupById(this.rangeId);
+    }
+  }
+})
+const app = createApp(App);
+app.mount('#app');
