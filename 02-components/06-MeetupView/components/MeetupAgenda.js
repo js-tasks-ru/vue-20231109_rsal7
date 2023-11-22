cb358bd0e84ca14765bb14b601af101fe6857f1e
RuslanSalimov <mail-spam2012@yandex.ru>
2023-11-22 23:45:46 +0500

    5th Meetup View task complete

---
+++ 02-components/06-MeetupView/components/MeetupAgenda.js
@@ -0,0 +1,20 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
+
+export default defineComponent({
+  name: 'MeetupAgenda',
+  components: {MeetupAgendaItem},
+  props: {
+    agenda: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul class="agenda">
+      <li class="agenda__item" v-for='item in agenda' :key='item.id'>
+        <MeetupAgendaItem :agendaItem='item'/>
+      </li>
+
+    </ul>`,
+});
---
+++ 02-components/06-MeetupView/components/MeetupInfo.js
@@ -0,0 +1,48 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      required: true
+    },
+    place: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Number,
+      required: true
+    }
+  },
+  methods: {
+    formatAsLocalDate(timestamp) {
+      return new Date(timestamp).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      });
+    },
+
+    formatAsIsoDate(timestamp) {
+      return new Date(timestamp).toISOString().substring(0, 10);
+    },
+  },
+
+  template: `
+    <ul class="meetup-info">
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
+        {{organizer}}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
+        {{place}}
+      </li>
+      <li>
+        <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
+        <time :datetime="formatAsIsoDate(date)">{{formatAsLocalDate(date)}}</time>
+      </li>
+    </ul>`,
+});
