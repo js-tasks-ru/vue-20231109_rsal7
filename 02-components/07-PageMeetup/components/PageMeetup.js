c3d0dda350204d511eb730e24869f2fd755f76a4
RuslanSalimov <mail-spam2012@yandex.ru>
2023-11-24 16:59:10 +0500

    6th Page Meetup  task complete

--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,7 +1,8 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import { fetchMeetupById } from '../meetupService.js';
+import MeetupView from './MeetupView';
 
 export default defineComponent({
   name: 'PageMeetup',
@@ -9,18 +10,55 @@ export default defineComponent({
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  props: {
+    meetupId: {
+      required: true,
+      type: Number,
+    },
+  },
+
+  data() {
+    return {
+      currentMeetup: null,
+      isLoading: false,
+      errors: [],
+    };
+  },
+  
+  watch: {
+    meetupId: {
+      handler(newValue, oldValue) {
+        this.errors = [];
+        this.isLoading = true;
+        fetchMeetupById(newValue)
+          .then((data) => {
+            this.currentMeetup = data;
+            this.isLoading = false;
+          })
+          .catch(e => {
+            console.warn(e);
+            this.currentMeetup = null;
+            this.isLoading = false;
+            this.errors.push('Error: Meetup not found!');
+          });
+      },
+      immediate: true,
+    },
   },
 
   template: `
-    <div class="page-meetup">
-      <!-- meetup view -->
+    <div class='page-meetup'>
+    <MeetupView v-if='currentMeetup && !isLoading' :meetup='currentMeetup' />
 
-      <UiContainer>
-        <UiAlert>Загрузка...</UiAlert>
-      </UiContainer>
+    <UiContainer v-if='isLoading'>
+      <UiAlert>Загрузка...</UiAlert>
+    </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
-      </UiContainer>
+    <UiContainer v-if='errors.length > 0'>
+      <UiAlert v-for='item in errors'>{{ item }}</UiAlert>
+    </UiContainer>
     </div>`,
 });
