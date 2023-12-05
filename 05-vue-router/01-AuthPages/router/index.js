fa391109f74e4b6a905b187ddc1a86ad5bd2740d
RuslanSalimov <mail-spam2012@yandex.ru>
2023-12-05 12:26:17 +0500

    Task 01-AuthPages finish

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,6 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: () => import('../views/PageIndex.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/PageLogin.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/PageRegister.vue'),
+    },
+  ],
 });
