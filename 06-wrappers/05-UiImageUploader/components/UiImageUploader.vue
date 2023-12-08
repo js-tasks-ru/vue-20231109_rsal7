7b75764205a603ae75a28973dd3ef4ba3c07a4b8
RuslanSalimov <mail-spam2012@yandex.ru>
2023-12-08 17:19:42 +0500

    Task 05-UiImageUploader finish

--- 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
+++ 06-wrappers/05-UiImageUploader/components/UiImageUploader.vue
@@ -1,15 +1,125 @@
 <template>
-  <div class="image-uploader">
-    <label class="image-uploader__preview image-uploader__preview-loading" style="--bg-url: url('/link.jpeg')">
-      <span class="image-uploader__text">Загрузить изображение</span>
-      <input type="file" accept="image/*" class="image-uploader__input" />
+  <div class='image-uploader'>
+    <label
+      class='image-uploader__preview'
+      :class='{"image-uploader__preview-loading": currentState.imgClass}'
+
+      :style="filePreview && { '--bg-url': `url(${filePreview})` }"
+    >
+      <span class='image-uploader__text'>{{ currentState.message }}</span>
+      <input
+        ref='input'
+        type='file'
+        accept='image/*'
+        class='image-uploader__input'
+        v-bind='$attrs'
+        @[currentState.eventName].prevent='currentState.eventMethod'
+      />
     </label>
   </div>
 </template>
 
 <script>
+import * as vm from 'vm';
+
 export default {
   name: 'UiImageUploader',
+  props: {
+    preview: {
+      type: String,
+      required: false,
+    },
+    uploader: {
+      type: Function,
+    },
+  },
+  inheritAttrs: false,
+  emits: ['select', 'upload', 'remove', 'error'],
+  data() {
+    const _vm = this;
+    return {
+      states: {
+        'empty': {
+          name: 'empty',
+          message: 'Загрузить изображение',
+          eventName: 'change',
+          eventMethod: _vm.emptyHandler,
+          emitName: 'select',
+          imgClass: '',
+        },
+        'loading': {
+          name: 'loading',
+          message: 'Загрузка...',
+          eventName: 'click',
+          eventMethod: _vm.loadingHandler,
+          emitName: 'upload',
+          imgClass: 'image-uploader__preview-loading',
+        },
+        'filled': {
+          name: 'filled',
+          message: 'Удалить изображение',
+          eventName: 'click',
+          eventMethod: _vm.filledHandler,
+          emitName: 'remove',
+          imgClass: '',
+        },
+      },
+
+      filePreview: this.preview,
+      currentState: null,
+    };
+  },
+  methods: {
+    emptyHandler(event) {
+      const file = event.target.files[0];
+      this.emitState(file);
+      this.filePreview = URL.createObjectURL(file);
+      if (this.uploader) {
+        this.setState('loading');
+        this.uploadFile(file);
+      } else {
+        this.setState('filled');
+      }
+    },
+    loadingHandler(event) {
+      return false;
+    },
+    filledHandler(event) {
+      this.clearInput();
+      this.emitState();
+      this.setState('empty');
+    },
+    uploadFile(file) {
+      this.uploader(file)
+        .then(data => {
+          this.filePreview = data.image;
+          this.emitState(data);
+          this.setState('filled');
+        })
+        .catch(e => {
+          console.warn(e);
+          this.emitError(e);
+          this.setState('empty');
+          this.clearInput();
+        });
+    },
+    clearInput() {
+      this.$refs.input.value = '';
+      this.filePreview = null;
+    },
+    setState(state) {
+      this.currentState = this.states[state];
+    },
+    emitState(data = null) {
+      this.$emit(this.currentState.emitName, data);
+    },
+    emitError(e){
+      this.$emit('error', e);
+    }
+  },
+  beforeMount() {
+    this.currentState = this.preview ? this.states.filled : this.states.empty;
+  },
 };
 </script>
 
