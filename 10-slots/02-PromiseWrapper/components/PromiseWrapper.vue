ccda8a55f1d08a57869b8567bb4326c037ba5b68
RuslanSalimov <mail-spam2012@yandex.ru>
2024-01-16 23:09:20 +0500

    02-UiPromiseWrapper complete

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,13 @@
 <template>
-  <!-- -->
+  <div v-if='currentState==="loading"'>
+    <slot name='pending' />
+  </div>
+  <div v-else-if='currentState==="rejected"'>
+    <slot :error='error' name='rejected' />
+  </div>
+  <div v-else-if='currentState==="fullFilled"'>
+    <slot :result='result' name='fulfilled' />
+  </div>
 </template>
 
 <script>
@@ -10,6 +18,30 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+  data() {
+    return {
+      currentState: 'loading',
+      result:  null,
+      error: null,
+    };
+  },
+  watch: {
+    promise: {
+      immediate: true,
+      async handler() {
+        this.currentState = 'loading';
+        this.result = null;
+        this.error = null;
+        try {
+          this.result = await this.promise;
+          this.currentState = 'fullFilled';
+        } catch (e) {
+          this.currentState = 'rejected';
+          this.error = e;
+        }
+      },
     },
   },
 };
